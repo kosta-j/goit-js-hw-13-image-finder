c38b03de2e83ac3daac55569f6ee28af511e8a21
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-07-12 02:27:52 +0300

    pnotify added, small refactoring

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,7 +1,10 @@
 import '../sass/main.scss';
-// import fetchedImages from './apiService';
 import PixabayApiService from './apiService';
 import cardsTpl from '../template/cards';
+import { defaults, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaults.delay = 4000;
 
 const refs = {
   cardsContainer: document.querySelector('.js-cards'),
@@ -16,11 +19,32 @@ refs.loadMorebtn.addEventListener('click', onLoadMoreClick);
 
 async function onSearchSubmit(e) {
   e.preventDefault();
+
   pixabayApiService.resetPageNumber();
+
   pixabayApiService.query = e.currentTarget.elements.query.value;
+
+  // empty form submit validation
+  if (pixabayApiService.query === '') {
+    error({
+      title: 'Something went wrong',
+      text: 'Please make your query more specific',
+    });
+    return;
+  }
 
   try {
     const queryCards = await pixabayApiService.fetchImages();
+
+    // wrong request validation
+    if (queryCards.hits.length === 0) {
+      error({
+        title: 'Something went wrong',
+        text: 'Please make your query more specific',
+      });
+      return;
+    }
+
     clearImages();
     render(queryCards.hits);
     showLoadmoreBtn();
@@ -50,8 +74,4 @@ function clearImages() {
 
 function showLoadmoreBtn() {
   refs.loadMorebtn.style.display = 'flex';
-}
-
-function hideLoadmoreBtn() {
-  refs.loadMorebtn.style.display = 'none';
 }
